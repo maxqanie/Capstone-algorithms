975719805fde222c37e510e81afef384eb258fff
maxqanie <137828600+maxqanie@users.noreply.github.com>
2024-05-22 15:38:00 -0700

    Add files via upload

---
+++ Copy of networking.py
@@ -0,0 +1,34 @@
+import network
+import socket
+from time import sleep
+
+class Networking(object):
+    def __init__(self):
+        self.ssid = 'Kerry’s iPhone'
+        self.password = 'a1cwy4vqty1vy'
+
+    def connect(self):
+        self.wlan = network.WLAN(network.STA_IF)
+        self.wlan.active(True)
+        self.wlan.connect(self.ssid, self.password)
+        while self.wlan.isconnected() == False:
+            print('Waiting for connection...')
+            sleep(1)
+        print(self.wlan.ifconfig())
+
+    def createUDPSocket(self):
+        # Create a UDP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+    def createTCPSocket(self, server_address):
+        # Create a UDP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.sock.connect(server_address)
+            print("Connected to TCP server")
+        except:
+            print("Not connected")
+
+    def sendTCPPacket(self, data):
+        self.sock.send(data.encode('ascii'))
